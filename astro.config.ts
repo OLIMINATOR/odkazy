c28b6390610f0a71f2b23a4bd9a3de1bd1261387
OLIMINATOR <oliminator@protonmail.com>
2023-01-06 21:06:35 +0100

    feat: create config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,30 +1,30 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
-import vercel from "@astrojs/vercel/static";
-import compress from "astro-compress";
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+import vercel from '@astrojs/vercel/static';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-    //site: SITE.origin,
-    base: "/",
-    trailingSlash: "never",
-    output: "static",
-    integrations: [
-        tailwind(),
-        sitemap(),
-        image(),
-        compress({
-            css: true,
-            html: true,
-            img: false,
-            js: true,
-            svg: false,
+	//site: SITE.origin,
+	base: '/',
+	trailingSlash: 'never',
+	output: 'static',
+	integrations: [
+		tailwind(),
+		sitemap(),
+		image(),
+		compress({
+			css: true,
+			html: true,
+			img: false,
+			js: true,
+			svg: false,
 
-            logger: 1,
-        }),
-    ],
-    adapter: vercel(),
+			logger: 1,
+		}),
+	],
+	adapter: vercel(),
 });
---
+++ src/config.ts
@@ -0,0 +1,37 @@
+export const SITE = {
+	title: 'Oliminator Odkazy',
+	description: 'soon.',
+	url: 'https://odkazy.oliminator.net',
+	themeColor: '#FF8A00',
+};
+
+export const OPEN_GRAPH = {
+	image: {
+		src: 'soon',
+		alt: 'soon',
+	},
+	twitter: 'oliminator34',
+};
+
+export const BUTTONS = [
+	{
+		name: 'YouTube',
+		background: 'youtube',
+		href: '/',
+	},
+	{
+		name: 'Instagram',
+		background: 'twitch',
+		href: '/',
+	},
+	{
+		name: 'Instagram',
+		background: 'twitch',
+		href: '/',
+	},
+	{
+		name: 'Instagram',
+		background: 'twitch',
+		href: '/',
+	},
+];
