444c42f67a86d0fe9d121e3cc9effeed21e3e180
OLIMINATOR <oliminator@protonmail.com>
2023-01-06 20:45:30 +0100

    build: install astro compress

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,27 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import vercel from "@astrojs/vercel/static";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), sitemap(), image()],
+    //site: SITE.origin,
+    base: "/",
+    trailingSlash: "never",
     output: "static",
+    integrations: [
+        tailwind(),
+        sitemap(),
+        image(),
+        compress({
+            css: true,
+            html: true,
+            img: false,
+            js: true,
+            svg: false,
+
+            logger: 1,
+        }),
+    ],
     adapter: vercel(),
 });
