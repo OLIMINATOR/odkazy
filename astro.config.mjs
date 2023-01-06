1f4e924ae9903d9d0ea81889ee5635338d453339
OLIMINATOR <oliminator@protonmail.com>
2023-01-06 20:14:29 +0100

    build: install dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [tailwind(), sitemap(), image()],
+    output: "static",
+    adapter: vercel(),
+});
